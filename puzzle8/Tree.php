a0dfc67646959cb930fb91f4fe23669740f70032
Filipe Voges <filipe.vogesh@gmail.com>
2019-04-11 13:14:25 -0300

    Update Tree.php

--- puzzle8/Tree.php
+++ puzzle8/Tree.php
@@ -1,1 +1,20 @@
-<?php
+<?php 
+
+/**
+ * \Tree
+ *
+ * @author Filipe Voges <filipe.vogesh@gmail.com>
+ * @since 2019-04-11
+ * @version 1.0
+ */
+class Tree {
+  /**
+   * @var \Board $board
+   */
+	protected $board;
+  
+	/**
+	 * array $nodes
+	 */
+	protected $nodes;	
+}
