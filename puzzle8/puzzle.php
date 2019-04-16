75daaf516061878c8d2460b32011122f1f48b0e3
Filipe Voges <filipe.vogesh@gmail.com>
2019-04-16 15:43:32 -0300

    Update puzzle.php

--- puzzle8/puzzle.php
+++ puzzle8/puzzle.php
@@ -3,6 +3,9 @@
 //header("HTTP/1.1 501");
 //echo "<strong>501</strong> - Not Implemented";
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 require_once 'classes/Position.php';
 
 die('aqui');
