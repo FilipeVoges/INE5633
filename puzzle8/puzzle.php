931d1e77dd6d022f42d383378fefb66186d0590c
Filipe Voges <filipe.vogesh@gmail.com>
2019-04-16 15:50:33 -0300

    Update puzzle.php

--- puzzle8/puzzle.php
+++ puzzle8/puzzle.php
@@ -6,14 +6,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require_once 'classes/Position.php';
+require_once 'AStar.php';
 
-$a = new Position(1, 1);
-echo '<pre>';
-var_dump($a);	
-echo '</pre>';
-echo '<hr>';
-$b = new Position(1, 2);
-echo '<pre>';
-var_dump($b);	
-echo '</pre>';
+$aStar = new AStar();
+var_dump($aStar);
