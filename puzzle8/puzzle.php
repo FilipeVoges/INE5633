59b1b50287bdc8d62e0bb5547d0412231f2d5575
Filipe Voges <filipe.vogesh@gmail.com>
2019-04-16 15:41:26 -0300

    teste

--- puzzle8/puzzle.php
+++ puzzle8/puzzle.php
@@ -3,23 +3,6 @@
 //header("HTTP/1.1 501");
 //echo "<strong>501</strong> - Not Implemented";
 
-include_once './classes/Position.php';
+require_once './classes/Position.php';
 
-$a = new Position(1, 1);
-var_dump($a->get('identifier');
-$a = new Position(1, 2);
-var_dump($a->get('identifier');
-$a = new Position(1, 3);
-var_dump($a->get('identifier');
-$a = new Position(2, 1);
-var_dump($a->get('identifier');
-$a = new Position(2, 2);
-var_dump($a->get('identifier');
-$a = new Position(2, 3);
-var_dump($a->get('identifier');
-$a = new Position(3, 1);
-var_dump($a->get('identifier');
-$a = new Position(3, 2);
-var_dump($a->get('identifier');
-$a = new Position(3, 3);
-var_dump($a->get('identifier');
+die('aqui');
