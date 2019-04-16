fdecb4a72726079b5346d036e73e62302575b5b5
Filipe Voges <filipe.vogesh@gmail.com>
2019-04-16 10:53:18 -0300

    Create AStar.php

---
+++ puzzle8/AStar.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once './classes/Board.php';
+include_once './HManhattan.php';
+
+/**
+ * \AStar
+ * Algoritmo A*
+ *
+ * @author Filipe Voges <filipe.vogesh@gmail.com>
+ * @since 2019-04-16
+ * @version 1.0
+ */
+class AStar {
+}
